4f1ace9553b46290ee3dd62864c666dad51e59a5
Andy Liu <andyliuliming@outlook.com>
2019-07-31 00:10:41 +0800

    separate folder.

---
+++ mockrprefsplits/go.mod
@@ -0,0 +1,5 @@
+module github.com/andyliuliming/gogomodule2/mockrprefsplits
+
+go 1.12
+
+require github.com/andyliuliming/gogomodule/mockoverlaymgrsplits/client v0.0.1
---
+++ mockrprefsplits/mockrprefsplits.go
@@ -0,0 +1,12 @@
+package main
+import (
+	"github.com/andyliuliming/gogomodule/mockoverlaymgrsplits/client"
+)
+
+func RP() {
+	client.ClientFunc()
+}
+
+func main() {
+	RP()
+}
